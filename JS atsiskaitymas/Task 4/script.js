5c7f405ac2baf3ec62dcb84c0af39db6bcf76909
batym24 <baty.mantas@gmail.com>
2023-04-11 09:44:36 +0300

    4 uzduotis

--- JS atsiskaitymas/Task 4/script.js
+++ JS atsiskaitymas/Task 4/script.js
@@ -10,3 +10,28 @@ bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+fetch(ENDPOINT)
+.then(res => res.json())
+.then(data => {
+    // console.log(data)
+    data.forEach(brand => {
+        const brandDiv = document.createElement('div')
+        brandDiv.classList.add('brand')
+
+        const brandHeader = document.createElement('h1')
+        brandHeader.appendChild(document.createTextNode(brand.brand))
+
+        const modelList = document.createElement('ul')
+        brand.models.forEach(car => {
+            const carModel = document.createElement('li')
+            carModel.appendChild(document.createTextNode(car))
+            modelList.appendChild(carModel)
+
+        })
+
+        brandDiv.append(brandHeader, modelList)
+
+        document.querySelector('#output').appendChild(brandDiv)
+    })
+})
