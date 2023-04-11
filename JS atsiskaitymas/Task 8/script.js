c677890b506d3c2608772117a994624096cde1e7
batym24 <baty.mantas@gmail.com>
2023-04-11 10:48:35 +0300

    8 uzduotis

--- JS atsiskaitymas/Task 8/script.js
+++ JS atsiskaitymas/Task 8/script.js
@@ -5,3 +5,19 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator{
+    sum(num1, num2){
+        return num1 + num2
+    }
+    subtraction(num1, num2){
+        return num1 - num2
+    }
+    multiplication(num1, num2){
+        return num1 * num2
+    }
+    division(num1, num2){
+        return num1 / num2
+    }
+}
+
